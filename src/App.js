fcaa84f5d063390114e382e0225855f323b155ef
Sunil Ballani <ballanisunil123@gmail.com>
2022-06-27 16:43:26 +0530

    Created various components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-import React from "react"
-
+import { useState } from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/feedbackData"
 function App(){
-    const title = 'Blog Post';
-    const body = 'This is my blog post'
-    const comments = [
-        {id:1, text: 'Comment One'},
-        {id:2, text: 'Comment Two'},
-        {id:3, text: 'Comment three'},
-    ]
-    const loading = false
-    const showComments = true
-    const commentBlock = (
-        <div className="comments">
-                <h3>Comments({comments.length})</h3>
-                <ul>
-                    {comments.map((comment, index)=>(
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-        </div>
-    )
-    if(loading) return <h1>Loading... </h1>
+
+    const[feedback, setFeedback] = useState(FeedbackData)
 
     return(
+        <>
+        <Header/>
         <div className="container">
-            <h1>{title}</h1>
-            <p>{body}</p>
-
-            {showComments && commentBlock}
-
-          
-        </div> 
+            <FeedbackList feedback = {feedback}/>
+        </div>
+        </> 
     )
 }
 export default App
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,21 @@
+import FeedbackItem from "./FeedbackItem"
+import PropTypes from 'prop-types'
+
+function FeedbackList({feedback}) {
+    if(!feedback || feedback.length === 0){
+        return <p> No feedback yet! </p>
+    }
+    return (
+        <div className="feedback-list">
+            {feedback.map((item)=>(
+               <FeedbackItem key = {item.id}  item={item}/>
+            ))}
+        </div>
+    )
+}
+
+FeedbackList.propTypes = {
+    feedback : PropTypes.array.isRequired
+}
+
+export default FeedbackList
